19ad8caa3970987024f971658292d36788035713
DDeRusha <dderusha@gmail.com>
2016-07-06 15:13:56 -0500

    added comments

--- FWR_Scratch.py
+++ FWR_Scratch.py
@@ -6,9 +6,9 @@ class fwrClass1_tk(Tkinter.Tk):
         Tkinter.Tk.__init__(self,parent)
         self.parent = parent
         self.initialize()
-
+# there are different classes (called layout managers) capable of placing the widgets in containers in different ways. Some are more flexible than others. Each container (window, pane, tab, dialog...) can have its own layout manager.the grid layout manager. It's a simple grid where you put your widgets, just like you would place things in a spreadsheet (Excel, OpenOffice Calc...).For example: Put a button at column 2, row 1.  Pur a checkbox at column 5, row 3.  etc.
     def initialize(self):
-        self(grid)
+        self.grid()
 
 if __name__ == "__main__":
     app = fwrClass1_tk(None)
